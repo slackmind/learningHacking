a79fe74c50aae759e2e6a4d8cf0fbbbb18e57b50
slackmind <j44.young@connect.qut.edu.au>
2020-04-27 09:37:06 +1000

    simple hello world loop

---
+++ firstprog.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// prints "Hello, world!"" once per line on ten lines
+int main() {
+    for (int i = 0; i< 10; i++) {
+        printf("Hello, world!\n");
+    }
+    return 0;
+}
